d0e3a74be246e9b43189d1c2d3a39fce357a146d
emlai <emil@cs.helsinki.fi>
2017-05-19 20:54:27 +0300

    Rename mushroom to ship / player

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,6 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Ship'
+import Ship from '../sprites/Ship'
 
 export default class extends Phaser.State {
   init () {}
@@ -13,19 +13,19 @@ export default class extends Phaser.State {
     game.cursors = game.input.keyboard.createCursorKeys()
     game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ])
 
-    this.mushroom = new Mushroom({
+    this.player = new Ship({
       game: this,
       x: this.world.centerX,
       y: this.world.centerY,
       asset: 'mushroom'
     })
 
-    this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.player)
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.spriteInfo(this.player, 32, 32)
     }
   }
 }
