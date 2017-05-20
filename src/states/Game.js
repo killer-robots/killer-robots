6241a9e39721a7991c2941d61e3b232c484710b2
emlai <emil@cs.helsinki.fi>
2017-05-20 18:29:54 +0300

    Add passive robots
    TODO: Make them shoot the player.

--- src/states/Game.js
+++ src/states/Game.js
@@ -43,6 +43,7 @@ export default class extends Phaser.State {
     fuelTextImage.fixedToCamera = true
 
     this.asteroids = game.add.physicsGroup();
+    this.robots = game.add.physicsGroup();
     this.explosions = game.add.group();
     this.explosions.createMultiple(10, 'kaboom');
     this.explosions.forEach(this.setupExplosion, this);
@@ -57,6 +58,8 @@ export default class extends Phaser.State {
 
   update() {
     this.addAsteroid();
+    this.addRobot();
+
       if (game.physics.arcade.collide(this.player, this.asteroids, this.collisionHandler, this.processHandler, this))
       {
           console.log('boom');
@@ -94,42 +97,40 @@ export default class extends Phaser.State {
     }
   }
 
-  addAsteroid() {
-    var chanceOfAsteroid = 0.1;
+  getRandomBorderSpawnPosition(randomValue, randomChance) {
+    var xPos = 0;
+    var yPos = 0;
+    if (randomValue <= randomChance / 4)
+    {
+      xPos = this.world.width * Math.random();
+      yPos = 0;
+    }
+    else if (randomValue <= randomChance / 2) {
+      xPos = this.world.width * Math.random();
+      yPos = this.world.height-1;
+    }
+    else if (randomValue <= (randomChance*3) /4 ) {
+      xPos = 0;
+      yPos = this.world.height * Math.random();
+    }
+    else {
+      xPos = this.world.width-1;
+      yPos = this.world.height * Math.random();
+    }
 
-    var asteroidRandom = Math.random();
-    if (asteroidRandom < chanceOfAsteroid) {
-      var xPos = 0;
-      var yPos = 0;
-      if (asteroidRandom <= chanceOfAsteroid / 4)
-      {
-        xPos = this.world.width * Math.random();
-        yPos = 0;
-      }
-      else if (asteroidRandom <= chanceOfAsteroid / 2) {
-        xPos = this.world.width * Math.random();
-        yPos = this.world.height-1;
-      }
-      else if (asteroidRandom <= (chanceOfAsteroid*3) /4 ) {
-        xPos = 0;
-        yPos = this.world.height * Math.random();
-      }
-      else {
-        xPos = this.world.width-1;
-        yPos = this.world.height * Math.random();
-      }
+    return new Phaser.Point(xPos, yPos);
+  }
 
-      var newAsteroid =  this.asteroids.create(xPos, yPos, 'asteroid', 0);
-
+  getInitialBorderSpawnVelocity(pos) {
       var baseSpeed = 100
       var speedX = 0
       var speedY = 0
-      if (xPos== 0) {
+      if (pos.x == 0) {
         speedX = baseSpeed * Math.random()
       } else {
         speedX = -baseSpeed * Math.random()
       }
-      if (yPos == 0) {
+      if (pos.y == 0) {
         speedY = baseSpeed * Math.random()
       } else {
         speedY = -baseSpeed * Math.random()
@@ -137,8 +138,17 @@ export default class extends Phaser.State {
 
       var randomAngle = Math.atan2(speedY, speedX) / (Math.PI / 180)
 
-      var direction = new Phaser.Point(speedX, speedY)
-      newAsteroid.body.velocity = direction
+      return new Phaser.Point(speedX, speedY)
+  }
+
+  addAsteroid() {
+    var chanceOfAsteroid = 0.1;
+
+    var asteroidRandom = Math.random();
+    if (asteroidRandom < chanceOfAsteroid) {
+      var pos = this.getRandomBorderSpawnPosition(asteroidRandom, chanceOfAsteroid);
+      var newAsteroid =  this.asteroids.create(pos.x, pos.y, 'asteroid', 0);
+      newAsteroid.body.velocity = this.getInitialBorderSpawnVelocity(pos);
 
       var randomAngle = 45 + Phaser.Math.radToDeg(
           Phaser.Point.angle(
@@ -156,12 +166,15 @@ export default class extends Phaser.State {
     }
   }
 
+  addRobot() {
+    var chanceOfRobot = 0.1;
 
-  render () {
-    var x = 32
-    var y = 32
-
-    this.fuelText.text = 'fuel:' + Math.round(this.player.fuel / this.player.fuelMax * 100) + '%'
+    var robotRandom = Math.random();
+    if (robotRandom < chanceOfRobot) {
+      var pos = this.getRandomBorderSpawnPosition(robotRandom, chanceOfRobot);
+      var newRobot = this.robots.create(pos.x, pos.y, 'robot', 0);
+      newRobot.body.velocity = this.getInitialBorderSpawnVelocity(pos);
+      newRobot.outOfBoundsKill = true;
+    }
   }
-
 }
